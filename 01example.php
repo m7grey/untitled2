9bdba93aeb13407a7c8b7ca774f8fdec896e92b7
m7grey <m7grey@gmail.com>
2017-02-11 14:01:00 -0500

    Initial commit

---
+++ 01example.php
@@ -0,0 +1,10 @@
+<h1> Example1 </h1>
+
+<?php
+
+//an example of a for loop
+for ($i = 0; $i < 10; $i++)
+    echo "<br /> I love code";
+</body>
+</html>
+
---
+++ 02example.php
@@ -0,0 +1,17 @@
+<h1> Example2 </h1>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+
+//an example of a for loop
+for ($i = 0; $i < 10; $i++)
+    echo "<div> $1 </div>div>";
+?>
+</body>
+</html>
+
